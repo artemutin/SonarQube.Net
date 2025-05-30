7ab4dd276b4cea2f819970f41df0b7f24e2d59aa
artemutin <artemutin@yandex.ru>
2025-05-30 17:10:02 +1000

    added Bearer auth support

---
+++ src/SonarQube.Net/Common/Authentication/BearerAuthentication.cs
@@ -0,0 +1,11 @@
+﻿namespace SonarQube.Net.Common.Authentication;
+
+public class BearerAuthentication: AuthenticationMethod
+{
+    public BearerAuthentication(string token)
+    {
+        Token = token;
+    }
+
+    public string Token { get; }
+}
--- src/SonarQube.Net/Common/Authentication/FlurlRequestExtensions.cs
+++ src/SonarQube.Net/Common/Authentication/FlurlRequestExtensions.cs
@@ -7,10 +7,12 @@ namespace SonarQube.Net.Common.Authentication
 	{
 		public static IFlurlRequest WithAuthentication(this IFlurlRequest request, AuthenticationMethod auth)
 		{
-			if (auth.GetType() == typeof(BasicAuthentication))
+			if (auth is BasicAuthentication basic)
 			{
-				var basic = (BasicAuthentication)auth;
 				return request.WithBasicAuth(basic.UserName, basic.Password);
+			}else if (auth is BearerAuthentication bearer)
+			{
+				return request.WithOAuthBearerToken(bearer.Token);
 			}
 
 			throw new InvalidOperationException("Unknown authentication method");
